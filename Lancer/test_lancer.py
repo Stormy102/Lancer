9948f07125c78610fd97fc265d9941b8d273fe4d
Matthew Dawson <matt@darkwindowstudios.com>
2019-08-20 18:25:42 +0100

    Fixed failing tests

--- Lancer/test_lancer.py
+++ Lancer/test_lancer.py
@@ -6,22 +6,22 @@ from utils import *
 
 def test_closes_critical_program_not_installed():
     with pytest.raises(SystemExit):
-        ProgramInstalled("programthatdoesntexist", True)
+        ProgramInstalled("programthatdoesntexist", True, False)
         assert True
 
 def test_non_critical_program_not_installed():
-    assert ProgramInstalled("programthatdoesntexist", False) == False
+    assert ProgramInstalled("programthatdoesntexist", False, False) == False
 
 def test_setup():
-    Setup()
+    setup()
     assert os.path.exists("nmap")
 
 def test_quits_with_no_parameters():
     with pytest.raises(SystemExit):
-        ParseArguments()
+        parse_arguments()
         assert True
 
 def test_signal_handler_quits():
     with pytest.raises(SystemExit):
-        SignalHandler(None, None)
+        signal_handler(None, None)
         assert True

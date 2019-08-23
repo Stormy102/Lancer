91fc9c070784c17397418ef1ca52fbe7f8766b3a
Matthew Dawson <matthewd102@hotmail.com>
2019-08-23 19:24:27 +0100

    Nmap tests (Travis is awesome)

--- Lancer/tests/modules/test_nmap.py
+++ Lancer/tests/modules/test_nmap.py
@@ -1,10 +1,20 @@
 from modules import nmap
 
+import lancerargs
 import os
 import tempfile
 import pytest
 import io
 import sys
+
+def test_nmap():
+    lancerargs.parse_arguments(['-T', 'scanme.nmap.org'])
+    nmap.nmap_scan(False)
+
+
+def test_nmap_quiet():
+    lancerargs.parse_arguments(['-T', 'scanme.nmap.org'])
+    nmap.nmap_scan(True)
 
 
 def test_parse_down_nmap_scan():
